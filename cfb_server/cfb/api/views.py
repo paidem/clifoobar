8f1e1be27f14c73ce74c64a58aa09d85cc9fc23b
Pavel Paidem <pavel.paidem@insoft.lt>
2026-03-04 01:30:23 +0200

    Stage 1: upgrade backend to Django 4.2 LTS-compatible stack
    - bump Django/DRF/taggit/environ and related runtime dependencies
    
    - bump django-auth-ldap and python-ldap pins
    
    - remove deprecated USE_L10N and fix SnippetAccessPermission base class

--- cfb_server/cfb/api/views.py
+++ cfb_server/cfb/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.response import Response
 from taggit.models import Tag
 
@@ -26,7 +26,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 # noinspection PyUnusedLocal
-class SnippetAccessPermission(BaseException):
+class SnippetAccessPermission(BasePermission):
     @staticmethod
     def has_permission(request, view):
         # Allow read to any users
--- cfb_server/cfb_server/settings.py
+++ cfb_server/cfb_server/settings.py
@@ -150,7 +150,6 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = env('DJANGO_TIME_ZONE', default="UTC")
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 # Override time formats
